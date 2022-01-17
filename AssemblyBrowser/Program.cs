11623e1d29e8c02f13904a9deb7fefcdd2fddaf1
darya-cherr <chernikovadarya67@gmail.com>
2022-01-18 02:25:46 +0300

    Create AssemblyBrowser project

---
+++ AssemblyBrowser/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AssemblyBrowser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Hello");
+        }
+    }
+}
