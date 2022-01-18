9502413a03565f67650510a1086df8b08db181ae
darya-cherr <chernikovadarya67@gmail.com>
2022-01-18 03:31:26 +0300

    Added get methods in ReaderLibrary

---
+++ AssemblyBrowser/ReaderLibrary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyBrowser
+{
+    public class ReaderLibrary : IReader
+    {
+        private Assembly asm;
+        
+        private MethodInfo[] GetMethods(Type type)
+        {
+            return type.GetMethods();
+        }
+        
+        private FieldInfo[] GetFields(Type type)
+        {
+            return type.GetFields();;
+        }
+        
+        private PropertyInfo[] GetProperties(Type type)
+        {
+            return type.GetProperties();
+        }
+        
+        private Assembly LoadAssembly(string path)
+        {
+            return Assembly.LoadFrom(path);;
+        }
+        
+        private Type[] GetTypes()
+        {
+            return asm.GetTypes();
+        }
+        
+        public void GetResult(string path)
+        {
+            asm = LoadAssembly(path);
+            Type[] asmTypes = GetTypes();
+            
+        }
+    }
+}
