b80ec1afd03a022a15730c8ce5e9038d28c23442
darya-cherr <chernikovadarya67@gmail.com>
2022-01-18 07:03:22 +0300

    Added ViewModel and ButtonClickCommand in MVVM structure

---
+++ WpfApp/ViewModel/ButtonClickCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfApp.ViewModel
+{
+    public class ButtonClickCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<object, bool> _canExecute;
+        
+        public ButtonClickCommand(Action execute, Func<object, bool> canExecute = null)
+        {
+            this._execute = execute;
+            this._canExecute = canExecute;
+        }
+        
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute();
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
---
+++ WpfApp/ViewModel/ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+
+namespace WpfApp.ViewModel
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private readonly AssemblyBrowser.ReaderLibrary reader = new AssemblyBrowser.ReaderLibrary();
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        
+        
+    }
+}
