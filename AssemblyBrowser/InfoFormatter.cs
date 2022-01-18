d686017f5c6bb19519aeb6a1ba195efb5b5b2adb
darya-cherr <chernikovadarya67@gmail.com>
2022-01-18 05:00:35 +0300

    Create InfoFormatter

---
+++ AssemblyBrowser/InfoFormatter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace AssemblyBrowser
+{
+    public class InfoFormatter
+    {
+        public string _nameSpace;
+        public string _class;
+        public List<MethodInfo> _methods;
+        public List<PropertyInfo> _properties;
+        public List<FieldInfo> _fields;
+
+
+        public InfoFormatter()
+        {
+            _nameSpace = "";
+            _class = "";
+            _methods = new List<MethodInfo>();
+            _properties = new List<PropertyInfo>();
+            _fields = new List<FieldInfo>();
+        }
+        
+        
+    }
+    
+    
+}
